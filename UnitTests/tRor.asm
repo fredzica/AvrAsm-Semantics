2b556208828b1e22b39871565d9122b84c56f8af
fredzica <fredzica@gmail.com>
2014-08-17 18:23:34 +0200

    test file for ror instruction

---
+++ UnitTests/tRor.asm
@@ -0,0 +1,30 @@
+ldi R16, 255
+ror R16
+;@vreg R16, 0x7F
+;@vsreg 0x19
+
+ldi R16, -128
+ror R16
+;@vreg R16, 0xC0
+;@vsreg 0x0C
+
+ldi R16, 1
+ror R16
+;@vreg R16, 0x00
+;@vsreg 0x1B
+
+ldi R16, 0
+ror R16
+;@vreg R16, 0x80
+;@vsreg 0x0C
+
+ldi R16, 54
+ror R16
+;@vreg R16, 0x1B
+;@vsreg 0x00
+
+ldi R16, 64
+ror R16
+;@vreg R16, 0x20
+;@vsreg 0x00
+
