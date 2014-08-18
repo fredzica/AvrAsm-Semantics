44aebe7deb0cc865ae80c0952a478aa191136dd1
fredzica <fredzica@gmail.com>
2014-08-18 20:43:29 +0200

    flags of subtraction instructions and functions for common operations on SREG register

---
+++ UnitTests/tSubi.asm
@@ -0,0 +1,35 @@
+ldi R16, 0
+subi R16, -34
+;@vreg R16, 0x22
+;@vsreg 0x21
+
+ldi R16, 1
+subi R16, 123
+;@vreg R16, 0x86
+;@vsreg 0x35
+
+ldi R16, 143
+subi R16, 3
+;@vreg R16, 0x8C
+;@vsreg 0x14
+
+ldi R16, 10
+subi R16, 10
+;@vreg R16, 0x00
+;@vsreg 0x02
+
+ldi R16, 255
+subi R16, 34
+;@vreg R16, 0xDD
+;@vsreg 0x14
+
+ldi R16, 23
+subi R16, 38
+;@vreg R16, 0xF1
+;@vsreg 0x15
+
+ldi R16, -124
+subi R16, -23
+;@vreg R16, 0x9B
+;@vsreg 0x35
+
